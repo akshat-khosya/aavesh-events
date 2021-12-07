ee52abc8b36b08a7c63c91ca37475f479ae0a9f9
Akshat Mittal <akshatmittal2506@gmail.com>
2021-12-08 00:33:21 +0530

    Updated team.js
    Just re-formatted the file.

--- React/aavesh-events/src/components/team.js
+++ React/aavesh-events/src/components/team.js
@@ -1,124 +1,191 @@
 const people = [
-  {
-    name: "Bhavya Arora",
-    subtitle: "Executive Member @Aavesh",
-    social: [
-      { handle: "instgram", link: "https://www.instagram.com/__bhavyaarora_/" },
-
-      {
-        handle: "linkedin",
-        link: "https://www.linkedin.com/in/bhavya-arora-5b6978175/",
-      },
-      { handle: "github", link: "https://github.com/akshatmittal61" },
-      { link: "9680180736", handle: "phone" },
-      { handle: "email", link: "bhavya.arora.222000@gmail.com" },
-    ],
-    image:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQEYy9DXsvc3zw/profile-displayphoto-shrink_200_200/0/1598326852737?e=1644451200&v=beta&t=60pw1KdROgz9IkHi76gECiqJr2hweDTA3Y3AwkF5PVk",
-  },
-  {
-    name: "Jitaksh Kapoor",
-    subtitle: "Penetration Tester",
-    social: [
-      { handle: "instgram", link: "https://www.instagram.com/jitakshkapoor/" },
-
-      {
-        handle: "linkedin",
-        link: "https://www.linkedin.com/in/jitaksh-kapoor/?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAACkhb7YBOlJ2It4m6wNCDYncEuzgwQhTWVU",
-      },
-      { handle: "github", link: "https://github.com/akshatmittal61" },
-      { handle: "phone", phone: "7696166014" },
-      { handle: "email", link: "bhavya.arora.222000@gmail.com" },
-    ],
-    image:
-      "https://media-exp1.licdn.com/dms/image/C4E03AQHNNFg6nQdS6A/profile-displayphoto-shrink_200_200/0/1635225818974?e=1644451200&v=beta&t=vkfFHYKYpVh6w4-ff6BaW0phGXS7vH3CSXXPb0ZZaQA",
-  },
-  {
-    name: "Kashish Arora",
-    subtitle: "Software Developer",
-    social: [
-      { handle: "instgram", link: "https://www.instagram.com/__bhavyaarora_/" },
-
-      {
-        handle: "linkedin",
-        link: "https://www.linkedin.com/in/kashish-arora11?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BHvEJN5%2BCRlubL3GcFbhVIA%3D%3D",
-      },
-      { handle: "github", link: "https://github.com/akshatmittal61" },
-      { handle: "phone", link: "9501132476" },
-      { handle: "email", link: "kashisharora4110@gmail.com" },
-    ],
-    image:
-      "https://media-exp1.licdn.com/dms/image/C5603AQG1ybnvxySmGQ/profile-displayphoto-shrink_800_800/0/1615282256475?e=1644451200&v=beta&t=9QxuF4YpKY4HQUAOqAQSJfHNVn-0z1lI4nDGyum2Fes",
-  },
-  {
-    name: "Anjali Srivastava",
-    subtitle: "Placement Coordinator",
-    social: [
-      { handle: "instgram", link: "https://www.instagram.com/__bhavyaarora_/" },
-
-      {
-        handle: "linkedin",
-        link: "https://www.linkedin.com/in/anjali-srivastava-939852178/?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAACogx70BC8gdp87FQ4bpInyWZfIbwJDmhfA",
-      },
-      { handle: "github", link: "https://github.com/akshatmittal61" },
-      { handle: "phone", link: "9140771599" },
-      { handle: "email", link: "bhavya.arora.222000@gmail.com" },
-    ],
-    image:
-      "https://media-exp1.licdn.com/dms/image/C4E03AQGmW4hctPp_kw/profile-displayphoto-shrink_200_200/0/1624129452597?e=1644451200&v=beta&t=GqBMnsnN3HyCKeBxE8VBOMwHKWLnnKSShaeXpZ_PpVw",
-  },
-  {
-    name: "Pranjal Joshi",
-    subtitle: "Software Engineer",
-    social: [
-      {
-        handle: "instgram",
-        link: "https://www.instagram.com/pranjal_joshi_pj/",
-      },
-
-      {
-        handle: "linkedin",
-        link: "https://www.linkedin.com/in/pranjaljoshi05/?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAAB6mG4wBwZOpIcatc52yr9wJ5TVW5FXwjOg",
-      },
-      { handle: "github", link: "https://github.com/akshatmittal61" },
-      { handle: "phone", link: "8006980617" },
-      { handle: "email", link: "bhavya.arora.222000@gmail.com" },
-    ],
-    image:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQEYy9DXsvc3zw/profile-displayphoto-shrink_200_200/0/1598326852737?e=1644451200&v=beta&t=60pw1KdROgz9IkHi76gECiqJr2hweDTA3Y3AwkF5PVk",
-  },
-  {
-    name: "Umang Sharma",
-    subtitle: "Android Developer",
-    social: [
-      { handle: "instgram", link: "https://www.instagram.com/__bhavyaarora_/" },
-
-      {
-        handle: "linkedin",
-        link: "https://www.linkedin.com/in/0729/?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAAC0Y1i8BK5-ecT-Qwp3adKZcqCKBa-zlNrA",
-      },
-      { handle: "github", link: "https://github.com/akshatmittal61" },
-      { handle: "phone", link: "9877918681" },
-      { handle: "email", link: "itsumangsharma@gmail.com" },
-    ],
-    image:
-      "https://media-exp1.licdn.com/dms/image/C4E03AQFVq8vRo73D6w/profile-displayphoto-shrink_200_200/0/1629965786723?e=1644451200&v=beta&t=nc4diijh4F3BvNCbnMKWB22nSJguAXKBV1pqOmAG6CY",
-  },
-  {
-    name: "Shivanand Shukla",
-    subtitle: "Executive Member @Aavesh",
-    social: [
-      { handle: "instgram", link: "https://www.instagram.com/__bhavyaarora_/" },
-
-      {
-        handle: "linkedin",
-        link: "https://www.linkedin.com/in/shivanand-shukla-855382191/?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAAC0VTkEBKesM3nrr05r9NJDkyhFowbuKK1o",
-      },
-      { handle: "github", link: "https://github.com/akshatmittal61" },
-      { handle: "phone", link: "9521007325" },
-      { handle: "email", link: "shivanandisshukla@gmail.com" },
-    ],
-    image:
-      "https://media-exp1.licdn.com/dms/image/C4E03AQFcbE-Bj7hBIw/profile-displayphoto-shrink_200_200/0/1598529399950?e=1644451200&v=beta&t=o8VlX52Rg9fjDJFxb5Yx2w6o2cGya2vCAWaCNtji9Bo ",
-  },
+	{
+		name: "Bhavya Arora",
+		subtitle: "Executive Member @Aavesh",
+		social: [
+			{
+				handle: "instgram",
+				link: "https://www.instagram.com/__bhavyaarora_/"
+			},
+			{
+				handle: "linkedin",
+				link: "https://www.linkedin.com/in/bhavya-arora-5b6978175/",
+			},
+			{
+				handle: "github",
+				link: "https://github.com/akshatmittal61"
+			},
+			{
+				handle: "phone",
+				link: "9680180736"
+			},
+			{
+				handle: "email",
+				link: "bhavya.arora.222000@gmail.com"
+			},
+		],
+		image: "https://media-exp1.licdn.com/dms/image/C4D03AQEYy9DXsvc3zw/profile-displayphoto-shrink_200_200/0/1598326852737?e=1644451200&v=beta&t=60pw1KdROgz9IkHi76gECiqJr2hweDTA3Y3AwkF5PVk",
+	},
+	{
+		name: "Jitaksh Kapoor",
+		subtitle: "Penetration Tester",
+		social: [
+			{
+				handle: "instgram",
+				link: "https://www.instagram.com/jitakshkapoor/"
+			},
+			{
+				handle: "linkedin",
+				link: "https://www.linkedin.com/in/jitaksh-kapoor/?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAACkhb7YBOlJ2It4m6wNCDYncEuzgwQhTWVU",
+			},
+			{
+				handle: "github",
+				link: "https://github.com/akshatmittal61"
+			},
+			{
+				handle: "phone",
+				link: "7696166014"
+			},
+			{
+				handle: "email",
+				link: "bhavya.arora.222000@gmail.com"
+			},
+		],
+		image: "https://media-exp1.licdn.com/dms/image/C4E03AQHNNFg6nQdS6A/profile-displayphoto-shrink_200_200/0/1635225818974?e=1644451200&v=beta&t=vkfFHYKYpVh6w4-ff6BaW0phGXS7vH3CSXXPb0ZZaQA",
+	},
+	{
+		name: "Kashish Arora",
+		subtitle: "Software Developer",
+		social: [
+			{
+				handle: "instgram",
+				link: "https://www.instagram.com/__bhavyaarora_/"
+			},
+			{
+				handle: "linkedin",
+				link: "https://www.linkedin.com/in/kashish-arora11?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BHvEJN5%2BCRlubL3GcFbhVIA%3D%3D",
+			},
+			{
+				handle: "github",
+				link: "https://github.com/akshatmittal61"
+			},
+			{
+				handle: "phone",
+				link: "9501132476"
+			},
+			{
+				handle: "email",
+				link: "kashisharora4110@gmail.com"
+			},
+		],
+		image: "https://media-exp1.licdn.com/dms/image/C5603AQG1ybnvxySmGQ/profile-displayphoto-shrink_800_800/0/1615282256475?e=1644451200&v=beta&t=9QxuF4YpKY4HQUAOqAQSJfHNVn-0z1lI4nDGyum2Fes",
+	},
+	{
+		name: "Anjali Srivastava",
+		subtitle: "Placement Coordinator",
+		social: [
+			{
+				handle: "instgram",
+				link: "https://www.instagram.com/__bhavyaarora_/"
+			},
+			{
+				handle: "linkedin",
+				link: "https://www.linkedin.com/in/anjali-srivastava-939852178/?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAACogx70BC8gdp87FQ4bpInyWZfIbwJDmhfA",
+			},
+			{
+				handle: "github",
+				link: "https://github.com/akshatmittal61"
+			},
+			{
+				handle: "phone",
+				link: "9140771599"
+			},
+			{
+				handle: "email",
+				link: "bhavya.arora.222000@gmail.com"
+			},
+		],
+		image: "https://media-exp1.licdn.com/dms/image/C4E03AQGmW4hctPp_kw/profile-displayphoto-shrink_200_200/0/1624129452597?e=1644451200&v=beta&t=GqBMnsnN3HyCKeBxE8VBOMwHKWLnnKSShaeXpZ_PpVw",
+	},
+	{
+		name: "Pranjal Joshi",
+		subtitle: "Software Engineer",
+		social: [
+			{
+				handle: "instgram",
+				link: "https://www.instagram.com/pranjal_joshi_pj/",
+			},
+			{
+				handle: "linkedin",
+				link: "https://www.linkedin.com/in/pranjaljoshi05/?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAAB6mG4wBwZOpIcatc52yr9wJ5TVW5FXwjOg",
+			},
+			{
+				handle: "github",
+				link: "https://github.com/akshatmittal61"
+			},
+			{
+				handle: "phone",
+				link: "8006980617"
+			},
+			{
+				handle: "email",
+				link: "bhavya.arora.222000@gmail.com"
+			},
+		],
+		image: "https://media-exp1.licdn.com/dms/image/C4D03AQEYy9DXsvc3zw/profile-displayphoto-shrink_200_200/0/1598326852737?e=1644451200&v=beta&t=60pw1KdROgz9IkHi76gECiqJr2hweDTA3Y3AwkF5PVk",
+	},
+	{
+		name: "Umang Sharma",
+		subtitle: "Android Developer",
+		social: [
+			{
+				handle: "instgram",
+				link: "https://www.instagram.com/__bhavyaarora_/"
+			},
+			{
+				handle: "linkedin",
+				link: "https://www.linkedin.com/in/0729/?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAAC0Y1i8BK5-ecT-Qwp3adKZcqCKBa-zlNrA",
+			},
+			{
+				handle: "github",
+				link: "https://github.com/akshatmittal61"
+			},
+			{
+				handle: "phone",
+				link: "9877918681"
+			},
+			{
+				handle: "email",
+				link: "itsumangsharma@gmail.com"
+			},
+		],
+		image: "https://media-exp1.licdn.com/dms/image/C4E03AQFVq8vRo73D6w/profile-displayphoto-shrink_200_200/0/1629965786723?e=1644451200&v=beta&t=nc4diijh4F3BvNCbnMKWB22nSJguAXKBV1pqOmAG6CY",
+	},
+	{
+		name: "Shivanand Shukla",
+		subtitle: "Executive Member @Aavesh",
+		social: [
+			{
+				handle: "instgram",
+				link: "https://www.instagram.com/__bhavyaarora_/"
+			},
+			{
+				handle: "linkedin",
+				link: "https://www.linkedin.com/in/shivanand-shukla-855382191/?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAAC0VTkEBKesM3nrr05r9NJDkyhFowbuKK1o",
+			},
+			{
+				handle: "github",
+				link: "https://github.com/akshatmittal61"
+			},
+			{
+				handle: "phone",
+				link: "9521007325"
+			},
+			{
+				handle: "email",
+				link: "shivanandisshukla@gmail.com"
+			},
+		],
+		image: "https://media-exp1.licdn.com/dms/image/C4E03AQFcbE-Bj7hBIw/profile-displayphoto-shrink_200_200/0/1598529399950?e=1644451200&v=beta&t=o8VlX52Rg9fjDJFxb5Yx2w6o2cGya2vCAWaCNtji9Bo ",
+	},
 ];
